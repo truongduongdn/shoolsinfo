63dd4dc8e36c8646e3e54e3f732f4d142d0410df
truongduongdn <166316007+truongduongdn@users.noreply.github.com>
2024-04-21 21:17:22 +0700

    new file:   src/component/Footer.js

---
+++ src/component/Footer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function Footer(props) {
+    return (
+        <div>
+            <div class="menu">
+  <div class="column">
+    <h2>Column 1</h2>
+    <ul>
+      <li><a href="#">Link 1</a></li>
+      <li><a href="#">Link 2</a></li>
+      <li><a href="#">Link 3</a></li>
+    </ul>
+  </div>
+  <div class="column">
+    <h2>Column 2</h2>
+    <ul>
+      <li><a href="#">Link 4</a></li>
+      <li><a href="#">Link 5</a></li>
+      <li><a href="#">Link 6</a></li>
+    </ul>
+  </div>
+  <div class="column">
+    <h2>Column 3</h2>
+    <ul>
+      <li><a href="#">Link 7</a></li>
+      <li><a href="#">Link 8</a></li>
+      <li><a href="#">Link 9</a></li>
+    </ul>
+  </div>
+</div>
+
+        </div>
+    );
+}
+
+export default Footer;
